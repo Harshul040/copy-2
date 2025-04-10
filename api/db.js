54273968608cfd63a3ce2fd5103d34a90fd909fa
Harshul040 <150358591+Harshul040@users.noreply.github.com>
2025-04-11 05:24:05 +0530

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql"
 
 export const db=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"$HarshulSoni04",
-    database:"blog"
+  connectionString: "postgresql://postgres.zijektluritjiogxsfkr:$HarshulSoni04@aws-0-ap-south-1.pooler.supabase.com:6543/postgres",
+    ssl: {
+        rejectUnauthorized: false // Required for Supabase connections
+    }
 
 });
 db.connect((err) => {
