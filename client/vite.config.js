18e5bc721773d4565f313058918fcf9767489c7b
Harshul040 <150358591+Harshul040@users.noreply.github.com>
2025-04-11 20:38:17 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,17 +8,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// No need for proxy since you're directly hitting the deployed backend URL
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      //'/api': 'http://localhost:8800',
-      '/api': {
-      target: 'http://localhost:8800',
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, ''), // Ensures the Host header matches the target
-    },
-    },
-  },
 });
+
