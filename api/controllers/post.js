25f5735311646757c4090d204e3707f6da31d09c
Harshul040 <150358591+Harshul040@users.noreply.github.com>
2025-04-11 18:59:43 +0530

    Update post.js

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -2,27 +2,32 @@ import { db } from "../db.js";
 import jwt from "jsonwebtoken";
 
 export const getPosts = (req, res) => {
-  const q = req.query.cat ? "SELECT * FROM posts WHERE cat=?" : "SELECT * FROM posts";
+  const q = req.query.cat
+    ? "SELECT * FROM posts WHERE cat = $1"
+    : "SELECT * FROM posts";
 
-  db.query(q, [req.query.cat], (err, data) => {
+  const params = req.query.cat ? [req.query.cat] : [];
+
+  db.query(q, params, (err, result) => {
     if (err) return res.status(500).send(err);
 
-    return res.status(200).json(data);
+    return res.status(200).json(result.rows);
   });
 };
-
 
 export const getPost = (req, res) => {
-  const q ="SELECT p.id, `username`, `title`, `desc`, p.img, u.img AS userImg, `cat`,`date` FROM user u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
+  const q = `
+    SELECT p.id, username, title, desc, p.img, u.img AS userImg, cat, date 
+    FROM "user" u 
+    JOIN posts p ON u.id = p.uid 
+    WHERE p.id = $1`;
 
-  db.query(q, [req.params.id], (err, data) => {
+  db.query(q, [req.params.id], (err, result) => {
     if (err) return res.status(500).json(err);
 
-    return res.status(200).json(data[0]);
+    return res.status(200).json(result.rows[0]);
   });
 };
-
-
 
 export const addPost = (req, res) => {
   const token = req.cookies.access_token;
@@ -37,7 +42,10 @@ export const addPost = (req, res) => {
       return res.status(403).json("Token is not valid!");
     }
 
-    const q = "INSERT INTO posts(`title`, `desc`, `img`, `cat`, `date`, `uid`) VALUES (?)";
+    const q = `
+      INSERT INTO posts(title, desc, img, cat, date, uid) 
+      VALUES ($1, $2, $3, $4, $5, $6)`;
+
     const values = [
       req.body.title,
       req.body.desc,
@@ -47,7 +55,7 @@ export const addPost = (req, res) => {
       userInfo.id,
     ];
 
-    db.query(q, [values], (err, data) => {
+    db.query(q, values, (err, result) => {
       if (err) {
         console.log("Database insertion error:", err);
         return res.status(500).json(err);
@@ -57,8 +65,6 @@ export const addPost = (req, res) => {
   });
 };
 
-
-
 export const deletePost = (req, res) => {
   const token = req.cookies.access_token;
   if (!token) return res.status(401).json("Not authenticated!");
@@ -67,16 +73,15 @@ export const deletePost = (req, res) => {
     if (err) return res.status(403).json("Token is not valid!");
 
     const postId = req.params.id;
-    const q = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?";
+    const q = "DELETE FROM posts WHERE id = $1 AND uid = $2";
 
-    db.query(q, [postId, userInfo.id], (err, data) => {
+    db.query(q, [postId, userInfo.id], (err, result) => {
       if (err) return res.status(403).json("You can delete only your post!");
 
       return res.json("Post has been deleted!");
     });
   });
 };
-
 
 export const updatePost = (req, res) => {
   const token = req.cookies.access_token;
@@ -85,13 +90,24 @@ export const updatePost = (req, res) => {
   jwt.verify(token, "jwtkey", (err, userInfo) => {
     if (err) return res.status(403).json("Token is not valid!");
 
-    const postId=req.params.id
+    const postId = req.params.id;
 
-    const q="UPDATE posts SET `title`=?, `desc`=?, `img`=?, `cat`=? WHERE `id`=? AND `uid`=?"
-    const values=[req.body.title,req.body.desc,req.body.img,req.body.cat]
+    const q = `
+      UPDATE posts 
+      SET title = $1, desc = $2, img = $3, cat = $4 
+      WHERE id = $5 AND uid = $6`;
 
-    db.query(q,[...values,postId,userInfo.id],(err,data)=>{
-      if(err) return res.status(500).json(err);
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.img,
+      req.body.cat,
+      postId,
+      userInfo.id,
+    ];
+
+    db.query(q, values, (err, result) => {
+      if (err) return res.status(500).json(err);
       return res.json("Post has been updated");
     });
   });
